3215b5ac63e7f973cca6ca010468cbd313834ff8
Matthieu Pignolet <m@mpgn.dev>
2025-03-30 00:02:31 +0400

    chore: add deployment script for the mpgn.dev network

---
+++ _deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm *.deb
+cp ../*.deb .
+while IFS= read -r dest; do
+  ssh -n "root@$dest" "rm pve-network-patch/*"
+  scp *.deb "root@$dest:pve-network-patch/"
+  ssh -n "root@$dest" "dpkg -i pve-network-patch/*.deb"
+done < proxmox_hosts
