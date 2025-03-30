a67db860c7063828b855d773a66146ef67acb238
Matthieu Pignolet <m@mpgn.dev>
2025-03-30 16:26:23 +0400

    sdn: evpn: make the NOT_DEFAULT routemap only apply when necessary
    Signed-off-by: Matthieu Pignolet <m@mpgn.dev>

--- src/PVE/Network/SDN/Controllers/EvpnPlugin.pm
+++ src/PVE/Network/SDN/Controllers/EvpnPlugin.pm
@@ -115,7 +115,6 @@ sub generate_controller_config {
     my $routemap = { rule => undef, action => "permit" };
     push(@{$config->{frr_routemap}->{'MAP_VTEP_IN'}}, $routemap );
     push(@{$config->{frr_routemap}->{'MAP_VTEP_OUT'}}, $routemap );
-    push(@{$config->{frr_routemap}->{'NOT_DEFAULT'}}, $routemap );
 
     return $config;
 }
@@ -206,20 +205,9 @@ sub generate_controller_zone_config {
 
     if ($is_gateway) {
 
+
 	$config->{frr_prefix_list}->{'only_default'}->{1} = "permit 0.0.0.0/0";
 	$config->{frr_prefix_list_v6}->{'only_default_v6'}->{1} = "permit ::/0";
-
-	# simple routemap to deny accepting default routes
-	my $routemap_config_v6 = ();
-	push @{$routemap_config_v6}, "match ipv6 address prefix-list only_default_v6";
-	my $routemap_v6 = { rule => $routemap_config_v6, action => "deny" };
-	unshift(@{$config->{frr_routemap}->{'NOT_DEFAULT'}}, $routemap_v6);
-
-	my $routemap_config = ();
-	push @{$routemap_config}, "match ip address prefix-list only_default";
-	my $routemap = { rule => $routemap_config, action => "deny" };
-	unshift(@{$config->{frr_routemap}->{'NOT_DEFAULT'}}, $routemap);
-
 
 	if (!$exitnodes_primary || $exitnodes_primary eq $local_node) {
 	    #filter default route coming from other exit nodes on primary node or both nodes if no primary is defined.
@@ -264,6 +252,15 @@ sub generate_controller_zone_config {
 		push @controller_config, "neighbor exitnode_local_routing route-map NOT_DEFAULT in";
 	    push(@{$config->{frr}->{router}->{"bgp $asn vrf $vrf"}->{"address-family"}->{"ipv4 unicast"}}, @controller_config);
 	    push(@{$config->{frr}->{router}->{"bgp $asn vrf $vrf"}->{"address-family"}->{"ipv6 unicast"}}, @controller_config);
+
+		my $notdefault = [
+	    	{ rule => ["match ipv6 address prefix-list only_default_v6"], action => "deny" },
+	    	{ rule => ["match ip address prefix-list only_default"], action => "deny" },
+	    	{ rule => undef, action => "permit" },
+		];
+	
+		$config->{frr_routemap}->{'NOT_DEFAULT'} = $notdefault;
+	
 	} else {
 	    @controller_config = ();
 	    #import /32 routes of evpn network from vrf1 to default vrf (for packet return)
